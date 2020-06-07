c483fcc06035dee86183373d37e943cfe6eaa3d9
Tania <tanialibramg@gmail.com>
2020-06-08 01:01:35 +0200

    hola mundo, comenzamos spa

--- 02-spa/src/app/app.module.ts
+++ 02-spa/src/app/app.module.ts
@@ -2,16 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { BodyComponent } from './components/body/body.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    BodyComponent,
-    FooterComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
