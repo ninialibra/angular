2f53bbd910b19215a989b919068f48680536558d
Tania <tanialibramg@gmail.com>
2020-06-12 00:09:14 +0200

    heroes, servicios

--- 02-spa/src/app/components/heroes/heroes.component.ts
+++ 02-spa/src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../servicios/heroes.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes:Heroe[] = [];
+
+  constructor(private _heroesService:HeroesService) {
+
+  }
 
   ngOnInit(): void {
+
+    this.heroes = this._heroesService.getHeroes();
+
+    console.log(this.heroes);
+    
+
   }
 
 }
