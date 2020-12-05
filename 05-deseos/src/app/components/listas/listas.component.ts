296803ce78efdaa19a26a239839fecdb7c3c5136
Tania <tanialibramg@gmail.com>
2020-12-05 20:24:52 +0100

    tab2 con componente

--- 05-deseos/src/app/components/listas/listas.component.ts
+++ 05-deseos/src/app/components/listas/listas.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DeseosService } from '../../services/deseos.service';
+import { Lista } from '../../models/lista.model';
 
 @Component({
   selector: 'app-listas',
@@ -7,8 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListasComponent implements OnInit {
 
-  constructor() { }
+  @Input() terminada = true;
+
+  constructor(public deseosService: DeseosService,
+              private router:Router) { }
 
   ngOnInit() {}
+
+  listaSeleccionada(lista:Lista){
+
+    if(this.terminada){
+      this.router.navigateByUrl(`/tabs/tab2/agregar/${lista.id}`);
+    }else{
+      this.router.navigateByUrl(`/tabs/tab1/agregar/${lista.id}`);
+    }
+    
+  }
+
+  borrarLista(lista:Lista){
+    this.deseosService.borrarLista(lista);
+  }
 
 }
