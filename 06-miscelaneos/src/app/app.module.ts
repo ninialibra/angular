2e86d9170b3e492cf20653bb9276710e8484851a
Tania <tanialibramg@gmail.com>
2020-12-08 20:24:01 +0100

    continue angular

--- 06-miscelaneos/src/app/app.module.ts
+++ 06-miscelaneos/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgStyleComponent } from './components/ng-style/ng-style.component';
+import { CssComponent } from './components/css/css.component';
+import { ClasesComponent } from './components/clases/clases.component';
+import { ResaltadoDirective } from './directives/resaltado.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgStyleComponent,
+    CssComponent,
+    ClasesComponent,
+    ResaltadoDirective
   ],
   imports: [
     BrowserModule,
