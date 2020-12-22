a6fcff8cc62fecf37a261b695a81ef9d1ca9c072
Tania <tanialibramg@gmail.com>
2020-12-22 20:07:36 +0100

    rutas hijas

--- 06-miscelaneos/src/app/app.routes.ts
+++ 06-miscelaneos/src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from "./components/home/home.component";
+import { UsuarioComponent } from './components/usuario/usuario.component';
+
+import { usuario_routes } from "./components/usuario/usuario.routes";
 
 const routes: Routes = [
-    { path: 'routePath', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { 
+        path: 'usuario/:id',
+        component: UsuarioComponent,
+        children: usuario_routes
+    },
     { path: '**', pathMatch:'full', redirectTo: 'routePath' }
 ];
 
