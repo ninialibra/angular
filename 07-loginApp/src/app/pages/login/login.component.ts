81f1acd0f253829d360f3e0d6a68f7857ffa0cbc
Tania <tanialibramg@gmail.com>
2021-01-03 17:02:19 +0100

    fin login, start forms

--- 07-loginApp/src/app/pages/login/login.component.ts
+++ 07-loginApp/src/app/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 import { UsuarioModel } from '../../models/usuario.model';
 
 @Component({
@@ -11,12 +13,18 @@ import { UsuarioModel } from '../../models/usuario.model';
 export class LoginComponent implements OnInit {
 
   usuario:UsuarioModel;
+  recordarme = false;
 
-  constructor(private auth:AuthService) { }
+  constructor(private auth:AuthService, private router:Router) { }
 
   ngOnInit() {
 
     this.usuario = new UsuarioModel();
+
+    if(localStorage.getItem('email')){
+      this.usuario.email = localStorage.getItem('email');
+      this.recordarme = true;
+    }
 
   }
 
@@ -26,11 +34,34 @@ export class LoginComponent implements OnInit {
       return;
     }
 
+    Swal.fire({
+      icon: 'info',
+      text: 'Espere por favor...',
+      allowOutsideClick: false
+    });
+
+    Swal.showLoading();
+
     this.auth.login(this.usuario)
       .subscribe(resp=>{
-        console.log(resp);        
+        console.log(resp);
+        
+        Swal.close();
+
+        if(this.recordarme){
+          localStorage.setItem('email',this.usuario.email);
+        }
+
+        this.router.navigateByUrl('/home'); 
+
       },(e)=>{
         console.log(e.error.error.message);
+
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: e.error.error.message
+        });
         
       });
 
--- 07-loginApp/src/app/pages/registro/registro.component.ts
+++ 07-loginApp/src/app/pages/registro/registro.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 import { UsuarioModel } from '../../models/usuario.model';
 
 @Component({
@@ -11,8 +13,9 @@ import { UsuarioModel } from '../../models/usuario.model';
 export class RegistroComponent implements OnInit {
 
   usuario:UsuarioModel;
+  recordarme = false;
 
-  constructor(private auth:AuthService) { }
+  constructor(private auth:AuthService, private router:Router) { }
 
   ngOnInit() { 
 
@@ -25,11 +28,34 @@ export class RegistroComponent implements OnInit {
       return;
     }
 
+    Swal.fire({
+      icon: 'info',
+      text: 'Espere por favor...',
+      allowOutsideClick: false
+    });
+
+    Swal.showLoading();
+
     this.auth.nuevoUsuario(this.usuario)
       .subscribe(resp=>{
-        console.log(resp);        
+        console.log(resp);   
+        Swal.close();     
+
+        if(this.recordarme){
+          localStorage.setItem('email',this.usuario.email);
+        }
+
+        this.router.navigateByUrl('/home');  
+
       },(e)=>{
         console.log(e.error.error.message);
+
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: e.error.error.message
+        });
+        
         
       });
     
