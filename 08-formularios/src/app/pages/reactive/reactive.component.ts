9300e7461a7adf75a0c5c91457ab9e1741db69bd
Tania <tanialibramg@gmail.com>
2021-01-05 20:39:24 +0100

    forms reactivos

--- 08-formularios/src/app/pages/reactive/reactive.component.ts
+++ 08-formularios/src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -13,22 +13,83 @@ export class ReactiveComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) {
 
     this.crearFormulario();
+    this.cargarDatosFormulario();
 
   }
 
   ngOnInit(): void {
   }
 
+  get pasatiempos(){
+    return this.formu.get('pasatiempos') as FormArray;
+  }
+
+  get nombreNoValido(){
+    return this.formu.get('nombre').invalid && this.formu.get('nombre').touched;
+  }
+
+  get apellidoNoValido(){
+    return this.formu.get('apellido').invalid && this.formu.get('apellido').touched;
+  }
+
+  get correoNoValido(){
+    return this.formu.get('correo').invalid && this.formu.get('correo').touched;
+  }
+
+  get calleNoValido(){
+    return this.formu.get('direccion.calle').invalid && this.formu.get('direccion.calle').touched;
+  }
+
+  get ciudadNoValido(){
+    return this.formu.get('direccion.ciudad').invalid && this.formu.get('direccion.ciudad').touched;
+  }
+
   crearFormulario() {
     this.formu = this.formBuilder.group({
-      nombre: [''],
-      apellido: [''],
-      correo: [''],
+      nombre: ['',[Validators.required, Validators.minLength(5)]],
+      apellido: ['',Validators.required],
+      correo: ['', [Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      direccion: this.formBuilder.group({
+        calle: ['',Validators.required],
+        ciudad: ['',Validators.required],
+      }),
+      pasatiempos: this.formBuilder.array([[],[],[]])
+    });
+  }
+
+  cargarDatosFormulario(){
+
+    //con setValue se tienen que mandar todas las propiedades. Con reset no haría falta
+    this.formu.reset({
+      nombre: "Cocos",
+      apellido: "Channel"/*,
+      correo: "",
+      direccion: {
+        calle: "La que sea",
+        ciudad: "Gijón"
+      }*/
     });
   }
 
   guardar(){
     console.log(this.formu);
+
+    if(this.formu.invalid){
+
+      return Object.values(this.formu.controls).forEach(control=>{
+
+        if(control instanceof FormGroup){
+          Object.values(control.controls).forEach(control=>control.markAsTouched);
+        }else{
+          control.markAsTouched();
+        }
+
+      });
+    }
+
+    this.formu.reset({
+      nombre: 'Sin nombre'
+    });
     
   }
 
