5a06892a7a860ce42b628931d97ca75a40a8d9e0
Tania <tanialibramg@gmail.com>
2021-01-10 21:08:14 +0100

    terminamos forms

--- 08-formularios/src/app/pages/reactive/reactive.component.ts
+++ 08-formularios/src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidadoresService } from '../../services/validadores.service';
 
 @Component({
   selector: 'app-reactive',
@@ -10,10 +11,11 @@ export class ReactiveComponent implements OnInit {
 
   formu: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private validadores:ValidadoresService) {
 
     this.crearFormulario();
     this.cargarDatosFormulario();
+    this.crearListeners();
 
   }
 
@@ -44,17 +46,51 @@ export class ReactiveComponent implements OnInit {
     return this.formu.get('direccion.ciudad').invalid && this.formu.get('direccion.ciudad').touched;
   }
 
+  get pass1NoValido(){
+    return this.formu.get('pass1').invalid && this.formu.get('pass1').touched;
+  }
+
+  get pass2NoValido(){
+    
+    const pass1 = this.formu.get('pass1').value;
+    const pass2 = this.formu.get('pass2').value;
+
+    return (pass1===pass2?false:true);
+  }
+
+  get usuarioNoValido(){
+    return this.formu.get('usuario').invalid && this.formu.get('usuario').touched;
+    
+  }
+
   crearFormulario() {
     this.formu = this.formBuilder.group({
       nombre: ['',[Validators.required, Validators.minLength(5)]],
-      apellido: ['',Validators.required],
+      apellido: ['',[Validators.required,this.validadores.noHerrera]],
       correo: ['', [Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      usuario: ['', , this.validadores.existeUsuario],
+      pass1: ['', [Validators.required]],
+      pass2: ['', [Validators.required]],
       direccion: this.formBuilder.group({
         calle: ['',Validators.required],
         ciudad: ['',Validators.required],
       }),
-      pasatiempos: this.formBuilder.array([[],[],[]])
+      pasatiempos: this.formBuilder.array([])
+    },{
+      validators: this.validadores.passEquals('pass1','pass2')
     });
+  }
+
+  crearListeners(){
+    this.formu.valueChanges.subscribe(valor=>{
+      console.log(valor);      
+    });
+
+    this.formu.statusChanges.subscribe(status=>{
+      console.log(status);      
+    });
+
+    this.formu.get('nombre').valueChanges.subscribe(console.log);
   }
 
   cargarDatosFormulario(){
@@ -62,13 +98,23 @@ export class ReactiveComponent implements OnInit {
     //con setValue se tienen que mandar todas las propiedades. Con reset no haría falta
     this.formu.reset({
       nombre: "Cocos",
-      apellido: "Channel"/*,
-      correo: "",
+      apellido: "Channel",
+      correo: "correo@correo.com",
+      pass1: "123",
+      pass2: "123",
       direccion: {
         calle: "La que sea",
         ciudad: "Gijón"
-      }*/
+      }
     });
+  } 
+
+  agregarPasatiempo(){
+    this.pasatiempos.push(this.formBuilder.control('',Validators.required));
+  }
+
+  borrarPasatiempo(i:number){
+    this.pasatiempos.removeAt(i);
   }
 
   guardar(){
