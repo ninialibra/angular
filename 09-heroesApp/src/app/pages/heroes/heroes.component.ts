abad2404ee0009f672e6db65481e207f4421cd07
Tania <tanialibramg@gmail.com>
2021-01-22 19:52:07 +0100

    heroesapp fin

--- 09-heroesApp/src/app/pages/heroes/heroes.component.ts
+++ 09-heroesApp/src/app/pages/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
 import { HeroeModel } from '../../models/heroe.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-heroes',
@@ -10,15 +11,37 @@ import { HeroeModel } from '../../models/heroe.model';
 export class HeroesComponent implements OnInit {
 
   heroes: HeroeModel[] = [];
+  cargando: boolean = false;
 
   constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
 
+    this.cargando = true;
+
     this.heroesService.getHeroes()
-      .subscribe(resp=>this.heroes = resp);
+      .subscribe(resp=>{
+        this.heroes = resp;
+        this.cargando = false;
+      });
   }
 
+  borrarHeroe(heroe:HeroeModel, i:number){
+    
+    Swal.fire({
+      title: 'Borrar registro',
+      text: `¿Está seguro de que quiere borrar a ${heroe.nombre}?`,
+      icon: 'question',
+      showConfirmButton: true,
+      showCancelButton: true
+    }).then(resp=>{
 
+      if(resp.value){
+        this.heroes.splice(i,1);    
+        this.heroesService.borrarHeroe(heroe.id).subscribe();
+      }
+
+    });    
+  }
 
 }
