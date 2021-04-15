04e6b9bc91e8bfd87da2fd1f3ca3e96e1b575a21
Tania <tanialibramg@gmail.com>
2021-04-15 23:11:40 +0200

    graficos fin

--- 16-graficos/src/app/app.module.ts
+++ 16-graficos/src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppComponent } from './app.component';
 import { ChartsModule } from 'ng2-charts';
 import { LineaComponent } from './components/linea/linea.component';
 import { BarraComponent } from './components/barra/barra.component';
+import { DonaComponent } from './components/dona/dona.component';
+import { RadarComponent } from './components/radar/radar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LineaComponent,
-    BarraComponent
+    BarraComponent,
+    DonaComponent,
+    RadarComponent
   ],
   imports: [
     BrowserModule,
