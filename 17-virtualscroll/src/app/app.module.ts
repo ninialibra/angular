86f1a5190257e8ea1826ec2504eff08c94108be3
Tania <tanialibramg@gmail.com>
2021-04-15 23:31:35 +0200

    scroll

--- 17-virtualscroll/src/app/app.module.ts
+++ 17-virtualscroll/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { VirtualComponent } from './components/virtual/virtual.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { VirtualComponent } from './components/virtual/virtual.component';
     VirtualComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
